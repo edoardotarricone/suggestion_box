3b3abb4757095a3734d173f548d6863d370efb7f
Edoardo Tarricone <tarricone.edoardo@gmail.com>
2016-07-15 15:43:14 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('SuggestionBox', []);
+app.config(function ($routeProvider) {
+  $routeProvider .when('/', {
+    controller: 'HomeController',
+    templateUrl: 'views/home.html'
+  }).otherwise({
+    templateUrl: '/'
+  });
+});
