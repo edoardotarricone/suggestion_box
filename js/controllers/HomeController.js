ceb209ccee996ed14bcc6ea6c69934b85c71366f
Edoardo Tarricone <tarricone.edoardo@gmail.com>
2016-07-15 16:10:25 +0200

    Update HomeController.js

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -4,4 +4,25 @@ app.controller('HomeController', [
  
 function($scope, suggestions) {
 	$scope.posts = suggestions.posts;
+	$scope.addSuggestion = function() {
+
+            // If input empty, don't submit. This is bound with ng-model
+            if (!$scope.title || $scope.title === "") {
+                return;
+            }
+
+            // Push suggestion posts in suggestions.js service
+            $scope.posts.push({
+                title: $scope.title,
+                upvotes: 0,
+                comments: [],
+                id: $scope.posts.length
+            });
+
+            // After pushing the data to the service, clear input
+            $scope.title = '';
+        };
+    $scope.upVote = function(post) {
+    	post.upvotes += 1;
+    };
 }]);
