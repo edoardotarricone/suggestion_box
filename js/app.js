362149df666dadddebf594892c9c583510ea8aaf
Edoardo Tarricone <tarricone.edoardo@gmail.com>
2016-07-15 17:39:21 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ app.config(function ($routeProvider) {
   })
   .when('/suggestion/:id', {
     controller: 'SuggestionController',
-    templateUrl: 'views/suggestion.html'
+    templateUrl: 'views/suggestions.html'
   })
   .otherwise({
     templateUrl: '/'
