f4a3f8974e7f403512674fae1852200b339c0db2
Edoardo Tarricone <tarricone.edoardo@gmail.com>
2016-07-15 18:57:05 +0200

    Update SuggestionController.js

--- js/controllers/SuggestionController.js
+++ js/controllers/SuggestionController.js
@@ -2,7 +2,7 @@ app.controller('SuggestionController', [
   '$scope', '$routeParams', 'suggestions',
   
   function($scope, $routeParams, suggestions){
-      $scope.post = suggestions.post[$routeParams.id];
+      $scope.post = suggestions.posts[$routeParams.id];
       
       $scope.addComment = function() {
         if (!$scope.body || $scope.body = "") {
